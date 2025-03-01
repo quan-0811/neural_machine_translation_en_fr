d66fc932c85bbab4b87bd8a8a568fa63cb171174
quan-0811 <quan.nh081104@gmail.com>
2025-03-01 14:22:49 +0700

    Add raw data handler

--- data_handler.py
+++ data_handler.py
@@ -1,11 +1,20 @@
+import torch
 import os
 import shutil
 import kagglehub
 import re
+from vocab import Vocabulary
 
 class EngFraData:
-    def __init__(self, download_folder_path = "data"):
+    def __init__(self, num_steps: int, download_folder_path = "data"):
         self.download_folder_path = download_folder_path
+        self.num_steps = num_steps
+        eng_tensor, fra_tensor, valid_eng_len, eng_vocab, fra_vocab = self.build_data()
+        self.eng_tensor = eng_tensor
+        self.fra_tensor = fra_tensor
+        self.valid_eng_len = valid_eng_len
+        self.eng_vocab = eng_vocab
+        self.fra_vocab = fra_vocab
 
     # Download the dataset
     def download(self):
@@ -54,13 +63,28 @@ class EngFraData:
     def tokenize(self, end_token = "<eos>"):
         src, tgt = [], []
         with open(self.download_folder_path + "/fra.txt", "r",  encoding="utf-8") as file:
-            for line in file:
+            lines = file.read().split("\n")
+            for line in lines:
                 parts = line.split("\t")
                 if len(parts) == 2:
                     src.append([word for word in f"{parts[0]} {end_token}".split(" ") if word])
                     tgt.append([word for word in f"{parts[1]} {end_token}".split(" ") if word])
         return src, tgt
 
+    # Pad/Truncate data to fixed length, turn into tensors
+    def build_data(self, start_token = "<bos>", pad_token = "<pad>"):
+        pad_or_trim = lambda seq, t: seq[:t] if len(seq) > t else seq + [pad_token] * (t - len(seq))
 
+        src, tgt = self.tokenize()
 
+        eng_sentences = [pad_or_trim(s, self.num_steps) for s in src]
+        fra_sentences = [[start_token] + pad_or_trim(s, self.num_steps) for s in tgt]
 
+        eng_vocab = Vocabulary(eng_sentences, min_freq=2)
+        fra_vocab = Vocabulary(fra_sentences, min_freq=2)
+
+        eng_tensor = torch.tensor([eng_vocab[s] for s in eng_sentences])
+        valid_eng_len = (eng_tensor != eng_vocab["<pad>"]).type(torch.int32).sum(1)
+        fra_tensor = torch.tensor([fra_vocab[s] for s in fra_sentences])
+
+        return eng_tensor, fra_tensor, valid_eng_len, eng_vocab, fra_vocab
