bb3880ecf29a11b2c9d99d24d381fed6abe1b99a
quan-0811 <quan.nh081104@gmail.com>
2025-03-04 07:11:45 +0700

    masked softmax logic fix

--- utils.py
+++ utils.py
@@ -26,9 +26,3 @@ def masked_softmax(X: torch.Tensor, valid_len: torch.Tensor):
     X = X * row_mask.unsqueeze(-1)
 
     return X
-
-X = torch.rand(size=(2, 5, 5))
-valid_lens = torch.tensor([2, 3])
-# valid_lens = torch.tensor([[2, 2, 0, 0, 0],
-#                            [3, 3, 3, 0, 0]])
-print(masked_softmax(X, valid_lens))
